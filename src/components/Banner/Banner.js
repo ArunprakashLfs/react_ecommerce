fc5ab3368991e43dcf3f5bfa9c256a1d271db267
kesavan <arunprakash7871@gmail.com>
2023-04-18 06:22:04 -0400

    added banner formate

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,35 @@
+// import {AiOutlineArrowRight, AiOutlineArrowLeft} from 'react-icons/all-files/ai'
+import {BsArrowLeft, BsArrowRight} from 'react-icons/bs'
 import './banner.scss'
+import { useState } from 'react'
 const Banner=({store})=>{
     let data = [...store]
+
+    //function for sliding
+    const [currentSlide,setCurrentSlide] = useState(0)
+    const prevSlide =()=>{
+        setCurrentSlide(currentSlide === 0?5:(prev)=>prev-1)
+    }
+    const nextSlide =()=>{
+        setCurrentSlide(currentSlide === 5?0:(prev)=>prev+1)
+    };
+    console.log(currentSlide)
     return(
         <div className="banner">
              {data.map(({images, brand})=>{
             return(
-                <div className='banner-img'>
-                    <div className='banner-img-container'>
-                        <img className='img-banner' src={images[1]} alt={brand}></img>
-                    </div>
+                <div className='banner-img' style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
                     <div>
-                        <div>
-                        AiOutlineArrowLeft
+                        <div className='banner-img-container'>
+                            <img className='img-banner' src={images[1]} alt={brand}></img>
+                        </div>
+                        <div className='Arrowcontainer'>
+                            <div onClick={prevSlide} className='arrow' >
+                                <BsArrowLeft />
+                            </div>
+                            <div onClick={nextSlide} className='arrow'>
+                                <BsArrowRight />    
+                            </div>
                         </div>
                     </div>
                 </div>
