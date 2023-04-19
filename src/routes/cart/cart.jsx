86b9210c956a0f21b44aad8383ebfffe60a0c6c6
kesavan <arunprakash7871@gmail.com>
2023-04-19 09:08:30 -0400

    Added cart details

--- src/routes/cart/cart.jsx
+++ src/routes/cart/cart.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+
 const Cart =()=>{
     const Data = useSelector((state)=>state.nithin.Data);
-    console.log(Data);
+    let cartData =[...Data]
+    let [baseQty, setBaseQty] = useState(1);
     return (
+        
         <div>
             
             <h2>Shopping cart</h2>
+            {cartData.map((val)=>{
+                return(
+                    <div>
+                        <div>
+                        <img src={val.images[0]} alt="img" />
+                        </div>
+                        <div>
+                            <p>{val.price}</p>
+                            {/* <p>Quantity<button>-</button><span>{val.quantity}</span><button>+</button></p> */}
+                             <p className='addtocart'>Quantity<button onClick={()=>setBaseQty(baseQty=== 1? (baseQty = 1):baseQty - 1 )}>-</button>{baseQty}<button onClick={()=>setBaseQty(baseQty + 1)}>+</button></p>
+                        </div>
+                    </div>
+                    
+                )
+                
+            })}
             
         </div>
     )
