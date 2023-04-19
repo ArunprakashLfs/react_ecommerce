ba55b20216edc4adede21ae8a85631cad5b71e8a
kesavan <arunprakash7871@gmail.com>
2023-04-19 08:57:20 -0400

    added cart proprty

--- src/routes/cart/cart.jsx
+++ src/routes/cart/cart.jsx
@@ -1,4 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
 const Cart =()=>{
+    const Data = useSelector((state)=>state.nithin.Data);
+    console.log(Data);
     return (
         <h2>Cart</h2>
     )
