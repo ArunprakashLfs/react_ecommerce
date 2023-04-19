4071e2e9c87640941fde58e4d2f73bcabace8642
kesavan <arunprakash7871@gmail.com>
2023-04-19 08:50:51 -0400

    Added property using reducers

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -18,8 +18,32 @@ export const slice= createSlice({
                 state.Data.push(action.payload);
             }
             
-        }
+        },
+        deleteFromCart:(state, action)=>{
+            state.Data = state.Data.filter(
+                (item) => item.id !== action.payload
+            );
+        },
+        resetCart: (state) =>{
+            state.Data= [];
+        },
+        increment: (state, action) =>{
+            const item = state.Data.find(
+                (item) => item.id === action.payload.id
+            );
+            if(item){
+                item.quantity++
+            }
+        },
+        decrement: (state, action) =>{
+            const item = state.Data.find(
+                (item) => item.id === action.payload.id
+            );
+            if(item){
+                item.quantity--
+            }
+        },
     }
 })
-export const {AddtoCart} = slice.actions;
+export const {AddtoCart,deleteFromCart,resetCart,increment,decrement} = slice.actions;
 export default slice.reducer;
