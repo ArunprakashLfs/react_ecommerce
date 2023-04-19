d5a4ef3109b61a02a758f675146731449337d07f
ArunprakashLfs <arunprakash7871@gmail.com>
2023-04-20 02:17:00 +0530

    Adding firebase components

--- src/routes/user/user.jsx
+++ src/routes/user/user.jsx
@@ -1,9 +1,20 @@
 import Footer from "../../components/footer/footer";
+import './user.scss'
 
 const User = ()=>{
     return(
-        <div>
-            <h2>User Page</h2>
+        <div >
+            {/* <h2>User Page</h2> */}
+            <div className="Userpage">
+                <div className="userlogin">
+                    <button className="google"><i class="fa-brands fa-google"></i>Login With Google</button>
+                    <button className="signout">Sign Out</button>
+                </div>
+                <div className="userlogin">
+                    <button className="linkedin"><i class="fa-brands fa-linkedin-in"></i>Login with LinkedIn</button>
+                    <button className="signout">SignOut</button>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
---
+++ src/utilities/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZrAngxLfpIQrLB8QDhCokDQrWwsK5IhM",
+  authDomain: "reactapp-26b9f.firebaseapp.com",
+  projectId: "reactapp-26b9f",
+  storageBucket: "reactapp-26b9f.appspot.com",
+  messagingSenderId: "200854246477",
+  appId: "1:200854246477:web:81f06863d062cdad653804",
+  measurementId: "G-NW26G67MD7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
