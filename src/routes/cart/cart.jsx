be3af83a624dc579069e105fcb896a628abcca28
ArunprakashLfs <arunprakash7871@gmail.com>
2023-04-20 00:41:37 +0530

    Aligned cart in css

--- src/routes/cart/cart.jsx
+++ src/routes/cart/cart.jsx
@@ -8,27 +8,43 @@ const Cart =()=>{
     let cartData =[...Data]
     let [baseQty, setBaseQty] = useState(1);
     return (
-        
-        <div className="cart-main">
+        <div className="main"> 
+            <div className="cart-main">
             
             <h2>Shopping cart</h2>
             {cartData.map((val)=>{
                 return(
-                    <div className="cart-container">
-                        <div className="cart-img-container">
-                        <img className="cart-img" src={val.images[0]} alt="img" />
+                    <div key ={val.id} className="cart-container">
+                        <div className="cart-items">
+                            <div className="cart-img-container">
+                                <img className="cart-img" src={val.images[0]} alt="img" />
+                            </div>
+                            {/* <div className="cart-content"> */}
+                                <p>{val.brand}</p>
+                                <p>${val.price}</p>
+                                {/* <p>Quantity<button>-</button><span>{val.quantity}</span><button>+</button></p> */}
+                                <p className='addtocart'>Quantity
+                                <button className="decrement" onClick={() => setBaseQty(baseQty === 1 ? (baseQty = 1) : baseQty - 1)}>-</button>
+                                {baseQty}
+                                <button className="increment" onClick={() => setBaseQty(baseQty + 1)}>+</button></p>
+
+                            {/* </div> */}
+                            <div className="remove">x</div>
                         </div>
-                        <div>
-                            <p>{val.price}</p>
-                            {/* <p>Quantity<button>-</button><span>{val.quantity}</span><button>+</button></p> */}
-                             <p className='addtocart'>Quantity<button onClick={()=>setBaseQty(baseQty=== 1? (baseQty = 1):baseQty - 1 )}>-</button>{baseQty}<button onClick={()=>setBaseQty(baseQty + 1)}>+</button></p>
-                        </div>
+                       
                     </div>
                     
                 )
                 
             })}
-            
+        
+        </div>
+        <div className="totalValue">
+                <h3>Total Amount</h3>
+                <p><strong>Total: ${ }</strong></p>
+                {/* <p>Shipping:<span>{}</span></p> */}
+                <button className="cart-button">Proceed to cart</button>
+        </div>
         </div>
     )
 }
